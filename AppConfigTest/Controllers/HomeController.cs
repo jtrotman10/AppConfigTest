d17026cd158eb9ffb88ecb8261ebca2e7973e3fc
Jeff Trotman <jtrotman@circlebox.com>
2022-07-01 12:34:05 -0500

    Add project files.

---
+++ AppConfigTest/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using AppConfigTest.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace AppConfigTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IConfiguration _config;
+
+        public HomeController(ILogger<HomeController> logger, IConfiguration config)
+        {
+            _logger = logger;
+            _config = config;
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.SampleConfigValue = _config["SampleConfigItem"];
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
